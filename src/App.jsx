bdee5d2e8ef700f573989829d7ce4dfd2e358d34
Orelba <88292816+Orelba@users.noreply.github.com>
2023-11-04 15:30:46 +0200

    Make the website fit well with the address bar in mobile browsers

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import ChatRoom from './components/ChatRoom/ChatRoom'
 import SignIn from './components/SignIn/SignIn'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW2dbA3FVZ6I4NbIHhloYHkR7X9mfk7og",
-  authDomain: "firechat-93661.firebaseapp.com",
-  projectId: "firechat-93661",
-  storageBucket: "firechat-93661.appspot.com",
-  messagingSenderId: "286467978930",
-  appId: "1:286467978930:web:59f8334d6277f0aebbfbab"
+  apiKey: 'AIzaSyCW2dbA3FVZ6I4NbIHhloYHkR7X9mfk7og',
+  authDomain: 'firechat-93661.firebaseapp.com',
+  projectId: 'firechat-93661',
+  storageBucket: 'firechat-93661.appspot.com',
+  messagingSenderId: '286467978930',
+  appId: '1:286467978930:web:59f8334d6277f0aebbfbab',
 }
 
 // Initialize Firebase
@@ -23,12 +23,8 @@ function App() {
   const [user] = useAuthState(auth)
 
   return (
-    <div className='interface'>
-      {user ? (
-        <ChatRoom auth={auth} db={db} />
-      ) : (
-        <SignIn auth={auth} />
-      )}
+    <div className="interface">
+      {user ? <ChatRoom auth={auth} db={db} /> : <SignIn auth={auth} />}
     </div>
   )
 }
